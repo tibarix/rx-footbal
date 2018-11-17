e38f8de0fadb929678788dc13499647d1789518e
tibarix <ayachi.mohammed94@gmail.com>
2018-11-17 10:08:51 +0100

    routing added

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,8 +1,14 @@
 import React, { Component, Fragment } from 'react'
 import { MuiThemeProvider } from '@material-ui/core/styles'
+import {
+  HashRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
 import theme from 'configs/config-theme'
 import PrimarySearchAppBar from '../../components/BusinessComponents/AppBar'
 import Home from 'containers/Home'
+import {Teams} from '../../components/BusinessComponents/Teams'
 import { appConfig } from 'configs/config-main'
 
 // global styles for entire app
@@ -14,7 +20,12 @@ class App extends Component {
       <MuiThemeProvider theme={theme}>
         <Fragment>
           <PrimarySearchAppBar>{appConfig.name}</PrimarySearchAppBar>
-          <Home />
+          <Router>
+            <Switch>
+              <Route exact path='/' component={Home} />
+              <Route path='/teams' component={Teams} />
+            </Switch>
+          </Router>
         </Fragment>
       </MuiThemeProvider>
     )
